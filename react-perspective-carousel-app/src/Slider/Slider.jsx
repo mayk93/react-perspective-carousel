54e2980e4181f4bb3caf92dbb03ed07e79362e97
michael <mihai@mandrescu.co>
2020-08-01 20:56:35 +0100

    Reworked rotational logic as collection of functions

--- react-perspective-carousel-app/src/Slider/Slider.jsx
+++ react-perspective-carousel-app/src/Slider/Slider.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGesture, useDrag } from 'react-use-gesture';
 import { useSprings, animated } from 'react-spring';
 
-let originalX = [], originalY = [], originalZ = [];
+// let originalX = [], originalY = [], originalZ = [];
 
 /* Start Utils */
 const shiftLeft = (array, shiftBy = 1) => array = array.concat(array.splice(0, shiftBy));
@@ -19,81 +19,105 @@ const clamp = (value, min, max) => {
 /* End Utils */
 
 /* Start Logic Utils */
-const springLogicBuilder = (visible, visibleSlidesPercentage) => {
-  let zIndex = 0;
-  const middle = visible / 2;
+const springLogicBuilder = ({ visible, total, offset, visibleSlidesPercentage }) => {
+  let start = offset;
+  let end = (offset + visible) % total - 1;
 
-  const getZIndex = () => {
-    const newZIndex = zIndex;
-    zIndex = zIndex - 1;
-    return newZIndex;
+  if (start > end) {
+    end = [start, start = end][0];
+  }
+
+  const middle = (start + end) / 2;
+
+  const fx = x => start <= x && x <= end ? x - offset : null;
+  const fy = x => {
+    if (start <= x && x <= middle) {
+      return x - offset;
+    } else if (x > middle && x <= end) {
+      return end - x;
+    }
+    return null;
   };
+  const fz = x => {
+    if (x === middle) {
+      return 0;
+    } else if (start <= x && x < middle) {
+      return -1 * (middle - x);
+    } else if (x > middle && x <= end) {
+      return -1 * (x - middle);
+    }
+    return null;
+  };
+  const fd = x => start <= x && x <= end ? 'initial' : 'none';
 
   const springLogic = index => {
-    const x = -index * visibleSlidesPercentage;
-    const y = index < middle ? index * visibleSlidesPercentage : (visible - index - 1) * visibleSlidesPercentage;
-    const z = index === parseInt(middle) ? 1 : index < middle ? 0 : getZIndex();
-    const display = index < visible ? 'initial' : 'none';
+    const x = fx(index);
+    const y = fy(index);
+    const z = fz(index);
+    const display = fd(index);
 
-    originalX[index] = x;
-    originalY[index] = y;
-    originalZ[index] = z;
+    const realX = -1 * visibleSlidesPercentage * x;
+    const realY = visibleSlidesPercentage * y;
 
-    return { x, y, zIndex: z, display, immediate: key => key === 'zIndex' ? true : false };
+    console.log({ x, y, z, display, realX, realY });
+
+    return { x: realX, y: realY, zIndex: z, display, immediate: key => key === 'zIndex' ? true : false };
   };
 
-  return springLogic;
+  return { springLogic, fx, fy, fz, fd };
 };
 
-const onDragBuilder = ({ index, items, setItems, spring, set }) => {
-  const onDrag = state => set(springIndex => {
-    if (springIndex === index) {
-      const { down, initial, movement, xy } = state;
-
-      const [ix] = initial;
-      const [mx] = movement;
-      const [x] = xy;
-
-      const newX = down ? mx + originalX[index] : originalX[index];
-
-      const newState = { x: newX };
-
-      const delta = ix - x;
-      const movedLeft = delta > 50;
-      const movedRight = delta < -50;
-
-      console.log('initial, movement, xy: ', initial, movement, xy);
-      console.log('delta: ', delta);
-      console.log({ movedLeft, movedRight });
-
-      if (movedLeft) {
-        setItems(shiftLeft(items));
-      } else if (movedRight) {
-        setItems(shiftRight(items));
-      }
-
-      return newState;
-    }
-  });
-
-  return onDrag;
-};
+// const onDragBuilder = ({ index, items, setItems, spring, set }) => {
+//   const onDrag = state => set(springIndex => {
+//     if (springIndex === index) {
+//       const { down, initial, movement, xy } = state;
+//
+//       const [ix] = initial;
+//       const [mx] = movement;
+//       const [x] = xy;
+//
+//       const newX = down ? mx + originalX[index] : originalX[index];
+//
+//       const newState = { x: newX };
+//
+//       const delta = ix - x;
+//       const movedLeft = delta > 50;
+//       const movedRight = delta < -50;
+//
+//       console.log('initial, movement, xy: ', initial, movement, xy);
+//       console.log('delta: ', delta);
+//       console.log({ movedLeft, movedRight });
+//
+//       if (movedLeft) {
+//         setItems(shiftLeft(items));
+//       } else if (movedRight) {
+//         setItems(shiftRight(items));
+//       }
+//
+//       return newState;
+//     }
+//   });
+//
+//   return onDrag;
+// };
 /* End Logic Utils */
 
 /* Start UI Components */
-const Card = ({ id, text, computedStyle, dragBind, x, y, zIndex, display }) => (
+const Card = ({ id, text, computedStyle, x, y, zIndex, display }) => (
   <animated.div
     key={id}
     className="slider-card"
     style={{ x, y, zIndex, display, ...computedStyle }}
-    {...dragBind()}
   >
     {text}
   </animated.div>
 );
 
 const Slider = ({ items, visible = 5 }) => {
-  const [usedItems, setItems] = useState([...items]);
+  console.log('Slider');
+  // const [usedItems, setItems] = useState([...items]);
+
+  const [offset, setOffset] = useState(0);
   const usedVisible = visible % 2 === 0 ? visible - 1 : visible;
   if (visible !== usedVisible) {
     console.warn(`Perspective carousel visible slides must be an odd number, using ${usedVisible} slides`);
@@ -102,23 +126,41 @@ const Slider = ({ items, visible = 5 }) => {
   const visibleSlidesPercentage = 100 / usedVisible;
   const computedStyleCard = { flex: `1 0 ${visibleSlidesPercentage}%` };
 
-  const [springs, set, stop] = useSprings(usedItems.length, springLogicBuilder(
-    usedVisible, visibleSlidesPercentage
-  ));
-  const dragBinds = usedItems.map((item, index) => useDrag(onDragBuilder({
-    index,
-    set,
-    setItems,
-    items: usedItems,
-    spring: springs[index]
-  })));
+  // const offset = 4 % items.length;
+
+  const { springLogic, fx, fy, fz, fd } = springLogicBuilder({
+    visible: usedVisible, total: items.length, visibleSlidesPercentage, offset
+  });
+  const [springs, set] = useSprings(items.length, springLogic);
+
+  useEffect(
+    () => {
+      set(index => springLogic(index));
+    }, // console.log('Effect called, springLogic, set: ', springLogic, fx, fy, fz, fd)
+    [offset]
+  );
+
+  // const dragBinds = usedItems.map((item, index) => useDrag(onDragBuilder({
+  //   index,
+  //   set,
+  //   setItems,
+  //   items: usedItems,
+  //   spring: springs[index]
+  // })));
+
+  console.log('Slider offset: ', offset);
 
   return (
-    <div className="slider-container">
-      {zip(usedItems, springs, dragBinds).map(([item, spring, dragBind]) => (
-        <Card key={item.id} computedStyle={computedStyleCard} dragBind={dragBind} {...item} {...spring} />
-      ))}
-    </div>
+    <>
+      <div className="slider-container">
+        {zip(items, springs).map(([item, spring]) => (
+          <Card key={item.id} computedStyle={computedStyleCard} {...item} {...spring} />
+        ))}
+      </div>
+      <button onClick={() => setOffset((offset + 1) % items.length)}>
+        Increase Offset
+      </button>
+    </>
   );
 };
 /* End UI Components */
